6aa17b364f079d54015140329114b7be4bfe64de
Frank Schwidom <schwidom@gmx.net>
2026-03-09 21:39:59 +0100

    Revert "add untracked : 2026-03-08 So 8. Mär 18:01:11 CET 2026 : updated title"
    This reverts commit 70025e58aa033ae4dc12c19e9ec491f353a0ba0d.

--- src/constants.rs
+++ src/constants.rs
@@ -1,3 +1,3 @@
-pub const HELP_QX: &str = "(e)dit, (w)rap, (q)uit, e(x)it";
+pub const HELP_QX: &str = "(w)rap, (q)uit, e(x)it";
 
-pub const HELP_FIRST_PAGE: &str = "(h)elp, (s)elect, (fF)lip, (v)iew, (e)dit, (w)rap, regex: (/)push (Esc)pop, (q)uit, e(x)it";
+pub const HELP_FIRST_PAGE: &str = "(h)elp, (s)elect, (fF)lip, (v)iew, (w)rap, regex: (/)push (Esc)pop, (q)uit, e(x)it";
