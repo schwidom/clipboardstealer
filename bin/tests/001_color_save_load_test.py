c4b5846eb6527f8d584de1130cd6b65aa34bf491
Frank Schwidom <schwidom@gmx.net>
2026-05-25 20:26:52 +0200

    add untracked : 2026-05-25 Mo 25. Mai 20:26:52 CEST 2026 : color save test

--- bin/tests/001_color_save_load_test.py
+++ bin/tests/001_color_save_load_test.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python3 -i
 
 import tempfile
+import os 
+
 
 with tempfile.TemporaryDirectory() as temp_dir:
     print("Temporary directory:", temp_dir)
+    os.system( f"cargo run -- --save-color-theme {temp_dir}/default.json")
+    os.system( f"cargo run -- --save-color-theme {temp_dir}/dracula.json -c dracula")
+
+    res = 0 == os.system( f"cmp {temp_dir}/default.json testdata/colorthemes/default.json")
+    print( f"default ok : {res}")
+
+    res = 0 == os.system( f"cmp {temp_dir}/dracula.json testdata/colorthemes/dracula.json")
+    print( f"dracula ok : {res}")
+
+    print(f"Temporary directory: {temp_dir}, press enter to clean up")
     # You can work with the directory here
-# Directory is automatically deleted after the block
+    input()
+    # Directory is automatically deleted after the block
 
 
